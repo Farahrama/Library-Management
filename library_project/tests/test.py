b60d5c90372ae3eb1e98b8ce2284e5ccb1b6171e
Farahrama <rafarah507@gmail.com>
2025-08-31 02:42:20 +0300

    add test

--- library_project/tests/test.py
+++ library_project/tests/test.py
@@ -5,3 +5,8 @@ class SmokeTest(TestCase):
     def test_homepage_runs(self):
         response = self.client.get("/")
         self.assertIn(response.status_code, [200, 404])
+from rest_framework.test import APITestCase
+class BookAPITest(APITestCase):
+    def test_list_books(self):
+        response = self.client.get('/api/books/')
+        self.assertEqual(response.status_code, 200)
